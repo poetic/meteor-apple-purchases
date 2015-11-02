58ac3589f7c45a577a133901378220a7a5359ce2
John Rodney <jrodney@poeticsystems.com>
2015-11-02 11:56:55 -0600

    add repo url to package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Meteor package for running apple in app purchasing inside of your mobile application.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/poetic/meteor-apple-purchases',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
